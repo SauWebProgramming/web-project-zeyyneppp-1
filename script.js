e197587ee97d72635d0024114c996b942af932cb
Zeynep Çiftçi <zeynep.ciftci2@ogr.sakarya.edu.tr>
2025-12-20 00:26:52 +0300

    En üste çıkma butonu ve yumuşak kaydırma özelliği eklendi.

--- script.js
+++ script.js
@@ -197,6 +197,22 @@ window.onclick = (event) => {
         document.body.style.overflow = 'auto';
     }
 };
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Yumuşak geçiş
+    });
+});
 
 /**
  * PROJEYİ BAŞLAT
